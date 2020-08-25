75ef23b89d98e75cb2478dcfde6b5b612915d0dd
jasperwreed <jmixon.reed@gmail.com>
2020-08-25 15:54:16 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Dog
+
+    def name 
+        @this_dogs_name
+    end
+
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+
+    
+
+    
+end
+
+lassie = Dog.new
+
+
+# binding.pry
+# 0
